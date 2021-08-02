fcb15bc4746c4d38c59851c293d491b6a1b1255b
felipendc <felipendc10@gmail.com>
2021-08-02 20:44:34 -0300

    initial update

---
+++ code_extensions.bat
@@ -0,0 +1,31 @@
+@REM INSTALL CODE EXTENTIONS FOR WINDOWS:
+
+@REM Windows CMD sintax.
+@REM call {"program_installation_path_(bin_folder)"} {--program_cmd_commands}
+
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension nico-castell.linux-desktop-file
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension erikphansen.vscode-toggle-column-selection
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension ritwickdey.LiveServer
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension DigitalBrainstem.javascript-ejs-support
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension dbaeumer.vscode-eslint
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension hex-ci.stylelint-plus
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension formulahendry.auto-close-tag
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension esbenp.prettier-vscode
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension ms-python.vscode-pylance
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension donjayamanne.python-extension-pack
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension almenon.arepl
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension PKief.material-icon-theme
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension Dart-Code.dart-code
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension naumovs.color-highlight
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension akamud.vscode-theme-onedark
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension Dart-Code.flutter
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension alexisvt.flutter-snippets
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension Gruntfuggly.todo-tree
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension CoenraadS.bracket-pair-colorizer-2
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension will-stone.plastic
+@REM call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension Nash.awesome-flutter-snippets
+@REM call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension TabNine.tabnine-vscode
+@REM call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension file-icons.file-icons
+
+@REM UNINSTALL CODE EXTENTIONS FOR WINDOWS:
+@REM call "C:\Program Files\Microsoft VS Code\bin\code" --uninstall-extension 
