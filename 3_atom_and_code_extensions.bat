fbb93982787744d616d2c29f27e48bdea1433356
felipendc <felipendc10@gmail.com>
2021-12-12 20:15:31 -0300

    Vicyos Rolling Update: (Sun 12/12/2021)

--- 3_atom_and_code_extensions.bat
+++ 3_atom_and_code_extensions.bat
@@ -3,46 +3,51 @@
 @REM Windows .bat sintax.
 @REM <pkg_bin_path> <args> <extensions>
 
+@REM Possible VSCode installation paths:
+@REM C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code
+@REM C:\Program Files\Microsoft VS Code\bin\code
+
+
 @REM This script will only work if Atom and VSCode has its PATHs already set up on Windows:
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension ms-vscode.cpptools
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension nico-castell.linux-desktop-file
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension erikphansen.vscode-toggle-column-selection
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension ritwickdey.LiveServer
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension DigitalBrainstem.javascript-ejs-support
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension dbaeumer.vscode-eslint
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension hex-ci.stylelint-plus
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension formulahendry.auto-close-tag
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension esbenp.prettier-vscode
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension ms-python.vscode-pylance
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension donjayamanne.python-extension-pack
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension almenon.arepl
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension PKief.material-icon-theme
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension Dart-Code.dart-code
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension naumovs.color-highlight
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension akamud.vscode-theme-onedark
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension Dart-Code.flutter
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension alexisvt.flutter-snippets
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension Gruntfuggly.todo-tree
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension CoenraadS.bracket-pair-colorizer-2
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension will-stone.plastic
-@REM call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension Nash.awesome-flutter-snippets
-@REM call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension TabNine.tabnine-vscode
-@REM call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension file-icons.file-icons
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension ms-vscode.cpptools
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension nico-castell.linux-desktop-file
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension erikphansen.vscode-toggle-column-selection
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension ritwickdey.LiveServer
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension DigitalBrainstem.javascript-ejs-support
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension dbaeumer.vscode-eslint
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension hex-ci.stylelint-plus
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension formulahendry.auto-close-tag
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension esbenp.prettier-vscode
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension ms-python.vscode-pylance
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension donjayamanne.python-extension-pack
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension almenon.arepl
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension PKief.material-icon-theme
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension Dart-Code.dart-code
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension naumovs.color-highlight
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension akamud.vscode-theme-onedark
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension Dart-Code.flutter
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension alexisvt.flutter-snippets
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension Gruntfuggly.todo-tree
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension CoenraadS.bracket-pair-colorizer-2
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension will-stone.plastic
+@REM call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension Nash.awesome-flutter-snippets
+@REM call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension TabNine.tabnine-vscode
+@REM call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension file-icons.file-icons
 
 @REM ## VSCode Extensions Angela Yu
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension ritwickdey.LiveServer
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension esbenp.prettier-vscode
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension formulahendry.auto-close-tag
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension hex-ci.stylelint-plus
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension dbaeumer.vscode-eslint
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension naumovs.color-highlight
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension DigitalBrainstem.javascript-ejs-support
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension erikphansen.vscode-toggle-column-selection
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension vscode-icons-team.vscode-icons
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension abusaidm.html-snippets
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension ms-azuretools.vscode-docker
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension kumar-harsh.graphql-for-vscode
-@REM call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension vscodevim.vim
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension ritwickdey.LiveServer
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension esbenp.prettier-vscode
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension formulahendry.auto-close-tag
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension hex-ci.stylelint-plus
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension dbaeumer.vscode-eslint
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension naumovs.color-highlight
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension DigitalBrainstem.javascript-ejs-support
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension erikphansen.vscode-toggle-column-selection
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension vscode-icons-team.vscode-icons
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension abusaidm.html-snippets
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension ms-azuretools.vscode-docker
+call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension kumar-harsh.graphql-for-vscode
+@REM call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension vscodevim.vim
 
 
 @REM # Atom
