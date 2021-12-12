1e18a2f7c67ac026afc3564f3639357139bf188e
felipendc <felipendc10@gmail.com>
2021-12-12 19:52:12 -0300

    Vicyos Rolling Update: (Sun 12/12/2021)

--- atom_and_code_extensions.bat
+++ atom_and_code_extensions.bat
@@ -1,62 +1,63 @@
 @REM INSTALL CODE AND ATOM EXTENTIONS ON WINDOWS:
 
 @REM Windows .bat sintax.
-@REM call {"program_installation_path_(.bin or .exe file)"} {.bin/.exe arguments}
+@REM pkg <args> <extensions>
 
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension ms-vscode.cpptools
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension nico-castell.linux-desktop-file
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension erikphansen.vscode-toggle-column-selection
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension ritwickdey.LiveServer
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension DigitalBrainstem.javascript-ejs-support
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension dbaeumer.vscode-eslint
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension hex-ci.stylelint-plus
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension formulahendry.auto-close-tag
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension esbenp.prettier-vscode
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension ms-python.vscode-pylance
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension donjayamanne.python-extension-pack
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension almenon.arepl
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension PKief.material-icon-theme
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension Dart-Code.dart-code
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension naumovs.color-highlight
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension akamud.vscode-theme-onedark
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension Dart-Code.flutter
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension alexisvt.flutter-snippets
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension Gruntfuggly.todo-tree
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension CoenraadS.bracket-pair-colorizer-2
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension will-stone.plastic
-@REM call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension Nash.awesome-flutter-snippets
-@REM call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension TabNine.tabnine-vscode
-@REM call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension file-icons.file-icons
+@REM This script will only work if Atom and VSCode has its PATHs already set up on Windows:
+code --install-extension ms-vscode.cpptools
+code --install-extension nico-castell.linux-desktop-file
+code --install-extension erikphansen.vscode-toggle-column-selection
+code --install-extension ritwickdey.LiveServer
+code --install-extension DigitalBrainstem.javascript-ejs-support
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension hex-ci.stylelint-plus
+code --install-extension formulahendry.auto-close-tag
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-python.vscode-pylance
+code --install-extension donjayamanne.python-extension-pack
+code --install-extension almenon.arepl
+code --install-extension PKief.material-icon-theme
+code --install-extension Dart-Code.dart-code
+code --install-extension naumovs.color-highlight
+code --install-extension akamud.vscode-theme-onedark
+code --install-extension Dart-Code.flutter
+code --install-extension alexisvt.flutter-snippets
+code --install-extension Gruntfuggly.todo-tree
+code --install-extension CoenraadS.bracket-pair-colorizer-2
+code --install-extension will-stone.plastic
+@REM code --install-extension Nash.awesome-flutter-snippets
+@REM code --install-extension TabNine.tabnine-vscode
+@REM code --install-extension file-icons.file-icons
 
 @REM ## VSCode Extensions Angela Yu
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension ritwickdey.LiveServer
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension esbenp.prettier-vscode
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension formulahendry.auto-close-tag
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension hex-ci.stylelint-plus
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension dbaeumer.vscode-eslint
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension naumovs.color-highlight
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension DigitalBrainstem.javascript-ejs-support
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension erikphansen.vscode-toggle-column-selection
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension vscode-icons-team.vscode-icons
-call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension abusaidm.html-snippets
-
-@REM UNINSTALL CODE EXTENTIONS FOR WINDOWS:
-@REM call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --uninstall-extension extention_name
+code --install-extension ritwickdey.LiveServer
+code --install-extension esbenp.prettier-vscode
+code --install-extension formulahendry.auto-close-tag
+code --install-extension hex-ci.stylelint-plus
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension naumovs.color-highlight
+code --install-extension DigitalBrainstem.javascript-ejs-support
+code --install-extension erikphansen.vscode-toggle-column-selection
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension abusaidm.html-snippets
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension kumar-harsh.graphql-for-vscode
+@REM code --install-extension vscodevim.vim
 
 
 @REM # Atom
-call "C:\Users\felip\AppData\Local\atom\bin\apm" install atom-ternjs
-call "C:\Users\felip\AppData\Local\atom\bin\apm" install autoclose-html-plus
-call "C:\Users\felip\AppData\Local\atom\bin\apm" install emmet
-call "C:\Users\felip\AppData\Local\atom\bin\apm" install csslint
-call "C:\Users\felip\AppData\Local\atom\bin\apm" install pigments
-call "C:\Users\felip\AppData\Local\atom\bin\apm" install language-ejs
-call "C:\Users\felip\AppData\Local\atom\bin\apm" install atom-beautify
-call "C:\Users\felip\AppData\Local\atom\bin\apm" install atom-html-preview
-call "C:\Users\felip\AppData\Local\atom\bin\apm" install Sublime-Style-Column-Selection
-call "C:\Users\felip\AppData\Local\atom\bin\apm" install linter-eslint
-call "C:\Users\felip\AppData\Local\atom\bin\apm" install atom-live-server-plus
-call "C:\Users\felip\AppData\Local\atom\bin\apm" install open-in-browsers
+apm install atom-ternjs
+apm install autoclose-html-plus
+apm install emmet
+apm install csslint
+apm install pigments
+apm install language-ejs
+apm install atom-beautify
+apm install atom-html-preview
+apm install Sublime-Style-Column-Selection
+apm install linter-eslint
+apm install atom-live-server-plus
+apm install open-in-browsers
 
 @REM UNINSTALL ATOM EXTENTIONS FOR WINDOWS:
-@REM call "C:\Users\felip\AppData\Local\atom\bin\apm" uninstall atom-ternjs
+
---
+++ dropped-temporary/atom_and_code_extensions.bat
@@ -0,0 +1,62 @@
+@REM INSTALL CODE AND ATOM EXTENTIONS ON WINDOWS:
+
+@REM Windows .bat sintax.
+@REM call {"program_installation_path_(.bin or .exe file)"} {.bin/.exe arguments}
+
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension ms-vscode.cpptools
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension nico-castell.linux-desktop-file
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension erikphansen.vscode-toggle-column-selection
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension ritwickdey.LiveServer
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension DigitalBrainstem.javascript-ejs-support
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension dbaeumer.vscode-eslint
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension hex-ci.stylelint-plus
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension formulahendry.auto-close-tag
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension esbenp.prettier-vscode
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension ms-python.vscode-pylance
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension donjayamanne.python-extension-pack
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension almenon.arepl
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension PKief.material-icon-theme
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension Dart-Code.dart-code
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension naumovs.color-highlight
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension akamud.vscode-theme-onedark
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension Dart-Code.flutter
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension alexisvt.flutter-snippets
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension Gruntfuggly.todo-tree
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension CoenraadS.bracket-pair-colorizer-2
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension will-stone.plastic
+@REM call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension Nash.awesome-flutter-snippets
+@REM call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension TabNine.tabnine-vscode
+@REM call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension file-icons.file-icons
+
+@REM ## VSCode Extensions Angela Yu
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension ritwickdey.LiveServer
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension esbenp.prettier-vscode
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension formulahendry.auto-close-tag
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension hex-ci.stylelint-plus
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension dbaeumer.vscode-eslint
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension naumovs.color-highlight
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension DigitalBrainstem.javascript-ejs-support
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension erikphansen.vscode-toggle-column-selection
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension vscode-icons-team.vscode-icons
+call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --install-extension abusaidm.html-snippets
+
+@REM UNINSTALL CODE EXTENTIONS FOR WINDOWS:
+@REM call "C:\Users\felip\AppData\Local\Programs\Microsoft VS Code\bin\code" --uninstall-extension extention_name
+
+
+@REM # Atom
+call "C:\Users\felip\AppData\Local\atom\bin\apm" install atom-ternjs
+call "C:\Users\felip\AppData\Local\atom\bin\apm" install autoclose-html-plus
+call "C:\Users\felip\AppData\Local\atom\bin\apm" install emmet
+call "C:\Users\felip\AppData\Local\atom\bin\apm" install csslint
+call "C:\Users\felip\AppData\Local\atom\bin\apm" install pigments
+call "C:\Users\felip\AppData\Local\atom\bin\apm" install language-ejs
+call "C:\Users\felip\AppData\Local\atom\bin\apm" install atom-beautify
+call "C:\Users\felip\AppData\Local\atom\bin\apm" install atom-html-preview
+call "C:\Users\felip\AppData\Local\atom\bin\apm" install Sublime-Style-Column-Selection
+call "C:\Users\felip\AppData\Local\atom\bin\apm" install linter-eslint
+call "C:\Users\felip\AppData\Local\atom\bin\apm" install atom-live-server-plus
+call "C:\Users\felip\AppData\Local\atom\bin\apm" install open-in-browsers
+
+@REM UNINSTALL ATOM EXTENTIONS FOR WINDOWS:
+@REM call "C:\Users\felip\AppData\Local\atom\bin\apm" uninstall atom-ternjs
